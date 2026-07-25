7b2bfc9cb9f7a659de5bf27ac6c58851016ccc3d
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-07-25 14:13:12 +0530

    adding changes

--- peering.tf
+++ peering.tf
@@ -27,21 +27,21 @@ resource "aws_vpc_peering_connection" "default" {
 
 
 resource "aws_route" "public_peering" {
-  count = is_peering_required ? 1 : 0
+  count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
 resource "aws_route" "private_peering" {
-  count = is_peering_required ? 1 : 0
+  count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
 resource "aws_route" "database_peering" {
-  count = is_peering_required ? 1 : 0
+  count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
@@ -49,7 +49,7 @@ resource "aws_route" "database_peering" {
 
 
 resource "aws_route" "default_peering" {
-  count = is_peering_required ? 1 : 0
+  count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.default.id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
